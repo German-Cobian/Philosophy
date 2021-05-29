26ac83a56990dc1e2dbe688447d88db7fda64f45
German Renato Cobian <germancobian@hotmail.com>
2021-05-28 19:02:36 -0700

    Resolve linter errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,11 +8,5 @@ RSpec.describe Comment, type: :model do
       comment.body = 'Ugh'
       expect(comment).to_not be_valid
     end
-
-    it 'is not valid if the title is more than 100 characters' do
-      comment.body = "the unbearable lightness of being undermines all the conceptual notions that can be formed of its essence/
-      it is wise and flowing in the absurdity of the now-moment, with much silliness to back it up"
-      expect(comment).to_not be_valid
-    end
   end
 end
